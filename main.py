ca0ea91d5211d68e199338f43e748b7526b314d4
a-abdulin <abdulin@list.ru>
2023-01-10 12:24:09 +0900

    Done

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! New day!!!'
 
 if __name__ == '__main__':
     app.run(host="localhost", port=10001, debug=True)
